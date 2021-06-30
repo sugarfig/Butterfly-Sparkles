a7f190f1c2d6fb81a6081de4d796b02ac1bbfc34
Erica Carballo <airwreckuh101@gmail.com>
2021-06-30 15:33:11 -0700

    butterfly moves after mouse clicks and holds

--- script_butterfly.js
+++ script_butterfly.js
@@ -2,6 +2,8 @@ let video;
 let latestPrediction = null;
 let modelIsLoading = true;
 let butterflyImage;
+let noseWidth;
+let noseLocation;
 
 //main points
 const NOSE_POINT = 195;
@@ -15,10 +17,10 @@ const RIGHT_NOSE = 456; //333 //371
 // const CHEEKS 
 
 //left eye
-const LEFT_EYE_LEFTSIDE = 33;
-const LEFT_EYE_RIGHTSIDE = 133;
-const LEFT_EYE_TOP = 159;
-const LEFT_EYE_BOTTOM = 143;
+// const LEFT_EYE_LEFTSIDE = 33;
+// const LEFT_EYE_RIGHTSIDE = 133;
+// const LEFT_EYE_TOP = 159;
+// const LEFT_EYE_BOTTOM = 143;
 
 
 //p5 function
@@ -31,6 +33,8 @@ function preload()
 function setup()
 {
     createCanvas(640, 480);
+    
+    
     video = createCapture(VIDEO);
     video.size(width, height);
 
@@ -49,8 +53,11 @@ function setup()
         //results[0]
         // console.log(results[0]);
         latestPrediction = results[0];
-        
+        //get nose location
+        noseLocation = latestPrediction.scaledMesh[NOSE_POINT];
     });
+
+    bug = new Butterfly();
 
     video.hide();
 }
@@ -68,55 +75,58 @@ function draw()
     if(latestPrediction == null) return;//don't draw anything else
 
     //get nose location
-    let noseLocation = latestPrediction.scaledMesh[NOSE_POINT];
-    let leftEyeLocation = latestPrediction.scaledMesh[LEFT_EYE];
+    noseLocation = latestPrediction.scaledMesh[NOSE_POINT];
+    // let leftEyeLocation = latestPrediction.scaledMesh[LEFT_EYE];
     
     console.log(noseLocation);
 
     let leftNoseLocation = latestPrediction.scaledMesh[LEFT_NOSE];
     let rightNoseLocation = latestPrediction.scaledMesh[RIGHT_NOSE];
 
-    let noseWidth = dist(leftNoseLocation[0], 
+    noseWidth = dist(leftNoseLocation[0], 
         leftNoseLocation[1], 
         rightNoseLocation[0], 
         rightNoseLocation[1]);
 
-    let leftEyeLeftLocation = latestPrediction.scaledMesh[LEFT_EYE_LEFTSIDE];
-    let leftEyeRightLocation = latestPrediction.scaledMesh[LEFT_EYE_RIGHTSIDE];
+    // let leftEyeLeftLocation = latestPrediction.scaledMesh[LEFT_EYE_LEFTSIDE];
+    // let leftEyeRightLocation = latestPrediction.scaledMesh[LEFT_EYE_RIGHTSIDE];
 
-    let leftEyeWidth = dist(leftEyeLeftLocation[0], 
-        leftEyeLeftLocation[1], 
-        leftEyeRightLocation[0], 
-        leftEyeRightLocation[1]);
+    // let leftEyeWidth = dist(leftEyeLeftLocation[0], 
+    //     leftEyeLeftLocation[1], 
+    //     leftEyeRightLocation[0], 
+    //     leftEyeRightLocation[1]);
 
-    let leftEyeTopLocation = latestPrediction.scaledMesh[LEFT_EYE_TOP];
-    let leftEyeBottomLocation = latestPrediction.scaledMesh[LEFT_EYE_BOTTOM];
+    // let leftEyeTopLocation = latestPrediction.scaledMesh[LEFT_EYE_TOP];
+    // let leftEyeBottomLocation = latestPrediction.scaledMesh[LEFT_EYE_BOTTOM];
 
-    let leftEyeHeight = dist(leftEyeTopLocation[0], 
-        leftEyeTopLocation[1],
-        leftEyeBottomLocation[0],
-        leftEyeBottomLocation[1]);
+    // let leftEyeHeight = dist(leftEyeTopLocation[0], 
+    //     leftEyeTopLocation[1],
+    //     leftEyeBottomLocation[0],
+    //     leftEyeBottomLocation[1]);
 
-    let butterflyWidth = noseWidth * 3;
-    let butterflyHeight = (butterflyImage.height / butterflyImage.width) * butterflyWidth;
+    // let pupilWidth = leftEyeWidth;
+    // let pupilHeight = leftEyeHeight / 2;
 
-    let pupilWidth = leftEyeWidth;
-    let pupilHeight = leftEyeHeight / 2;
+
+
+    if(mouseIsPressed) //eyebrow raise
+    {
+        bug.move();
+    }
+    else
+    {
+        bug.updateNoseLocation();
+    }
     
-    imageMode(CENTER);
-    image
-    (butterflyImage,
-    noseLocation[0], 
-    noseLocation[1], 
-    butterflyWidth, 
-    butterflyHeight);
+    bug.display();
+    
 
-    fill('rgba(100%,20%,70%,0.5)');
-    stroke('rgba(100%,20%,70%,0.5)');
-    ellipse(leftEyeLocation[0] + 10, 
-        leftEyeLocation[1] + 10, 
-        pupilWidth, 
-        pupilHeight);
+    // fill('rgba(100%,20%,70%,0.5)');
+    // stroke('rgba(100%,20%,70%,0.5)');
+    // ellipse(leftEyeLocation[0] + 10, 
+    //     leftEyeLocation[1] + 10, 
+    //     pupilWidth, 
+    //     pupilHeight);
 
     // image
     // (butterflyImage,
@@ -131,4 +141,57 @@ function draw()
     //     leftNoseLocation[1], 
     //     rightNoseLocation[0], 
     //     rightNoseLocation[1]);
+}
+
+
+//to move butterfly
+
+let bug; // Declare object
+// function setup() {
+//   createCanvas(710, 400);
+//   // Create object
+//   bug = new Butterfly();
+// }
+// function draw() {
+//   if (mouseIsPressed){
+//     bug.move();
+//   }
+//   bug.display();
+// }
+
+
+// Butterfly class
+class Butterfly 
+{
+  constructor() 
+  {
+    this.x = width / 2;
+    this.y = height / 2;
+    this.diameter = random(10, 30);
+    this.speed = 1;
+  }
+  move() 
+  {
+    this.x -= 1;
+    this.y -= random(0, 4);
+  }
+  display() 
+  {
+    let butterflyWidth = noseWidth * 3;
+    let butterflyHeight = (butterflyImage.height / butterflyImage.width) * butterflyWidth;
+    imageMode(CENTER);
+    image
+    (butterflyImage,
+    this.x, 
+    this.y, 
+    butterflyWidth, 
+    butterflyHeight);
+    // ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+
+  updateNoseLocation()
+  {
+    this.x = noseLocation[0];
+    this.y = noseLocation[1];
+  }
 }
